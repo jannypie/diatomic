f51e61a84d122bf11b087cd1189df1a8e20913b3
Jan Dennison <jldennison@gmail.com>
2015-04-12 13:21:11 -0400

    removed seed data from app.js, update to pass search_spec again

--- spec/features/search_spec.rb
+++ spec/features/search_spec.rb
@@ -1,6 +1,12 @@
 require 'spec_helper.rb'
 
 feature "Looking up experiments", js: true do
+  before do
+    Experiment.create!(title: 'Baked Potato w/ Cheese')
+    Experiment.create!(title: 'Garlic Mashed Potatoes')
+    Experiment.create!(title: 'Potatoes Au Gratin')
+    Experiment.create!(title: 'Baked Brussel Sprouts')
+  end
   scenario "finding experiments" do
     visit '/'
     fill_in "keywords", with: "baked"
