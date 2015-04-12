d7973724adb9c58d05fd8eb7169056d99750ade3
Jan Dennison <jldennison@gmail.com>
2015-04-12 10:59:57 -0400

    connect experiments controller to active record, with specs

--- db/migrate/20150412020228_create_experiments.rb
+++ db/migrate/20150412020228_create_experiments.rb
@@ -1,7 +1,8 @@
 class CreateExperiments < ActiveRecord::Migration
   def change
     create_table :experiments do |t|
-
+      t.string  :title, null: false
+      t.text    :description
       t.timestamps null: false
     end
   end
